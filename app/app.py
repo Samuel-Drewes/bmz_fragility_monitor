9a4d0cfd7b72c0e674ddc5accc486113d0b4c42d
Samuel Drewes <samueldrewes@Samuels-MacBook-Air-3.local>
2024-03-26 13:10:39 +0100

    frust tabs

--- app/app.py
+++ app/app.py
@@ -3,25 +3,8 @@ import numpy as np
 import pandas as pd
 
 st.title('My Application')
- 
-# Create tabs
-tab_titles = ['Topic A', 'Topic B', 'Topic C']
-tab1, tab2, tab3 = st.tabs(tab_titles)
- 
-# Add content to each tab
-with tab1:
-    st.header('Topic A')
-    st.write('Topic A content')
+st.write("Why is this failing?")
 
-    full_df = pd.read_csv("upload_data/full_df.csv")
+full_df = pd.read_csv("upload_data/full_df.csv")
 
-    st.dataframe(data=full_df)
- 
-with tab2:
-    st.header('Topic B')
-    st.write('Topic B content')
- 
-with tab3:
-    st.header('Topic C')
-    st.write('Topic C content')
-
+st.dataframe(data=full_df)
