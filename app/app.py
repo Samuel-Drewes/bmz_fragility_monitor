4d7fc6a54c20461559efb83b76816ed275e5660a
Samuel Drewes <samueldrewes@Samuels-MacBook-Air-3.local>
2024-03-26 16:03:34 +0100

    undo for now

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import geopandas as gpd
-import plotly.express as px
+# import geopandas as gpd
+# import plotly.express as px
 
 
 
@@ -17,31 +17,31 @@ clean_df = full_df\
 
 ## For the Map
 
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-geo_merge = world.merge(full_df, left_on = 'iso_a3', right_on = 'iso')
+# world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+# geo_merge = world.merge(full_df, left_on = 'iso_a3', right_on = 'iso')
 
-geo_merge = geo_merge.drop(columns=['pop_est', 'continent', 'name_x', 'iso_a3', 'gdp_md_est', 'country-code'])\
-    .rename(columns={'name_y': 'Country Name', 'weighted_mean': "Total Score"})\
-    .set_index('Country Name')
-geo_merge_2 = geo_merge.set_index('Country Name')
+# geo_merge = geo_merge.drop(columns=['pop_est', 'continent', 'name_x', 'iso_a3', 'gdp_md_est', 'country-code'])\
+#     .rename(columns={'name_y': 'Country Name', 'weighted_mean': "Total Score"})\
+#     .set_index('Country Name')
+# geo_merge_2 = geo_merge.set_index('Country Name')
 
-red_green_scale = [
-    [0, 'red'], 
-    [0.5, 'yellow'], 
-    [1, 'green']  
-]
+# red_green_scale = [
+#     [0, 'red'], 
+#     [0.5, 'yellow'], 
+#     [1, 'green']  
+# ]
 
-fig = px.choropleth(geo_merge_2,
-                    geojson=geo_merge_2.geometry,
-                    locations=geo_merge_2.index,
-                    color="Total Score",
-                    hover_name=geo_merge_2.index,  # or any column for names
-                    hover_data=['Total Score', 'Dimension_G', 'Dimension_S', 'Dimension_S', 'Dimension_I', 'Dimension_C',
-       'Dimension_E', 'Dimension_R'],  # Add more columns here
-                    projection="mercator",
-                    color_continuous_scale=red_green_scale  # Or use 'Plotly3' for a built-in option
-                    )
-fig.update_geos(fitbounds="locations", visible=False)
+# fig = px.choropleth(geo_merge_2,
+#                     geojson=geo_merge_2.geometry,
+#                     locations=geo_merge_2.index,
+#                     color="Total Score",
+#                     hover_name=geo_merge_2.index,  # or any column for names
+#                     hover_data=['Total Score', 'Dimension_G', 'Dimension_S', 'Dimension_S', 'Dimension_I', 'Dimension_C',
+#        'Dimension_E', 'Dimension_R'],  # Add more columns here
+#                     projection="mercator",
+#                     color_continuous_scale=red_green_scale  # Or use 'Plotly3' for a built-in option
+#                     )
+# fig.update_geos(fitbounds="locations", visible=False)
 
 # Overall Title
 
@@ -65,7 +65,7 @@ if page == 'Home':
     st.dataframe(data=compare_df)
 
     st.header('General Fragility Map')
-    st.plotly_chart(fig)
+    # st.plotly_chart(fig)
 
 
 elif page == 'Visualization 1':
