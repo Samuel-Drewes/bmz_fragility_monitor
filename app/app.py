efae60aec6e72c2b17f77ee30231d068e4807ee9
Samuel Drewes <samueldrewes@Samuels-MacBook-Air-3.local>
2024-03-26 13:06:28 +0100

    redo tabs?

--- app/app.py
+++ app/app.py
@@ -2,9 +2,26 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.write('Hello world! Schokolade!!!')
+st.title('My Application')
+ 
+# Create tabs
+tab_titles = ['Topic A', 'Topic B', 'Topic C']
+tab1, tab2, tab3 = st.tabs(tab_titles)
+ 
+# Add content to each tab
+with tab1:
+    st.header('Topic A')
+    st.write('Topic A content')
 
+    full_df = pd.read_csv("upload_data/full_df.csv")
 
-full_df = pd.read_csv("upload_data/full_df.csv")
+    st.dataframe(data=full_df)
+ 
+with tab2:
+    st.header('Topic B')
+    st.write('Topic B content')
+ 
+with tab3:
+    st.header('Topic C')
+    st.write('Topic C content')
 
-st.dataframe(data=full_df)
